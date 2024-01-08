10eec5198c4ca9e331d0edc6f23c4ea3fc3ece39
ranicocs <danielqueijo14@gmail.com>
2024-01-09 00:18:01 +0100

    Migrating to MVC pattern

--- server/src/index.js
+++ server/src/index.js
@@ -1,53 +1,14 @@
 import express, { json } from 'express'
-import cors from 'cors'
-export const app = express()
-const port = process.env.PORT ?? 3000 ?? 3001
+import { router } from './routes/movies.js'
+import { corsMiddleware } from './middlewares/cors.js'
 
+export const app = express()
 app.use(json())
-app.use(cors({
-    origin: (origin, callback) => {
-      const ACCEPTED_ORIGINS = [
-        'http://localhost:5171'
-      ]
-  
-      if (ACCEPTED_ORIGINS.includes(origin)) {
-        return callback(null, true)
-      }
-  
-      if (!origin) {
-        return callback(null, true)
-      }
-  
-      return callback(new Error('Not allowed by CORS'))
-    }
-}))
 app.disable('x-powered-by')
 
-import { router } from './routes/router.js'
+app.use('/api/v1/', contactsRouter)
 
-export const contacts = [
-    {
-        name: 'jose',
-        birthday: '2004-11-04',
-        phone: 643271289,
-        email: 'jose@joseando.com'
-    },
-    {
-        name: 'maria',
-        birthday: '1990-01-01',
-        phone: 618571534,
-        email: 'maria@gmail.com'
-    }
-]
-
-
-router(app, contacts)
-
-app.use((req, res) => {
-    res.status(404)
-        // .setHeader('Content-Type', 'text/html; charset=utf-8')
-        .send('404 Not Found😡')
-})
+const PORT = process.env.PORT ?? 3000
 
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`)
