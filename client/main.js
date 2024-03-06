c510c2d394212044a44f20998fd291b103e39ff7
ranicocs <danielqueijo14@gmail.com>
2024-03-06 18:33:02 +0100

    Add contacts fetching

--- client/main.js
+++ client/main.js
@@ -1,12 +1,48 @@
 window.addEventListener('load', () => {
-    fetch('http://localhost:3000/v1/contacts/jose/y', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-        .then((response) => response.json())
-        .then((contacts) => console.log(contacts))
-        .catch((error) => console.log(`error wtf ${error}`))
+    const contactBook = document.getElementById('contact-book')
 
+    const fetchContacts = async () => {
+        const contacts = await fetch('http://localhost:3000/v1/contacts/', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+
+        return await contacts.json()
+    }
+    
+    const renderContacts = (contacts) => {
+        let contactsHTML = ''
+
+        for (let contact of contacts) {
+            contactsHTML += `
+            <article class="contact">
+            <img src="logo.svg" alt="pipo" class="logo"/>
+            <aside>
+              <div class="contact-data">
+                <div><b>Name: </b> <span>${contact.name}</span></div>
+                <div><b>Birthday: </b> <span>${contact.birthday}</span></div>
+                <div><b>Tlf: </b> <span>${contact.phone}</span></div>
+                <div><b>Email: </b> <span>${contact.email}</span></div>
+              </div>
+              <div class="contact-btns">
+                <button class="edit-btn">
+                  <img src="edit-btn.svg" alt="edit" />
+                </button>
+                <button class="remove-btn">
+                  <img src="remove-btn.svg" alt="remove" />
+                </button>
+              </div>
+            </aside>
+          </article>
+            `
+        }
+        contactBook.innerHTML = contactsHTML
+    }
+
+    fetchContacts()
+        .then(contacts => renderContacts(contacts))
+        .catch(error => console.error(error))
 })
+
