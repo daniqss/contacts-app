592c68f62d27beb11b190ead33baee4418d8b380
ranicocs <danielqueijo14@gmail.com>
2024-04-12 16:47:12 +0200

    Finish ui

--- server/src/schemas/mongooseSchema.js
+++ server/src/schemas/mongooseSchema.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose'
 
 export const contactSchema = new mongoose.Schema({
     id: {
-        type: mongoose.Schema.Types.UUID,
-        unique: true
+        type: mongoose.Schema.Types.UUID//,
+        // unique: true
     },
     name: String,
     birthday: Date,
